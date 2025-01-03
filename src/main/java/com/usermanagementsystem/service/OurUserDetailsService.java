4c449784f434216629de6f4ada496e510d01cc1b
PhannyDoem <phanny.doem@student.cadt.edu.kh>
2025-01-03 14:48:39 +0700

    spring boot register and login

--- src/main/java/com/usermanagementsystem/service/OurUserDetailsService.java
+++ src/main/java/com/usermanagementsystem/service/OurUserDetailsService.java
@@ -1,4 +1,25 @@
 package com.usermanagementsystem.service;
 
-public class OurUserDetailsService {
+
+import com.usermanagementsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OurUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    /**
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
 }
